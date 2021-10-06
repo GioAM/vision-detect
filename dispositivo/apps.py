a2b2c27edf531a8c011859b9bc6193024b016c2b
Giovani Meneguel <34317514+GioAM@users.noreply.github.com>
2021-10-06 17:55:54 -0300

    adicionando pagina da camera

---
+++ dispositivo/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DispositivoConfig(AppConfig):
+    name = 'dispositivo'
--- visiondetect/urls.py
+++ visiondetect/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('objeto.urls')),
     path('', include('imagem.urls')),
+    path('', include('dispositivo.urls')),
     path('admin/', admin.site.urls),
 ]
